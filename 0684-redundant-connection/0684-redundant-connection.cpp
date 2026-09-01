a64afa0aa84d2e8891f14b075f2366ade084eba6
Garbhit Maheshwari <garbhitm@gmail.com>
2026-09-01 19:32:08 +0530

    Time: 3 ms (33.77%), Space: 13.4 MB (46.23%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> parent;
+
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    bool unite( int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if(a==b) return false;
+
+        parent[a] =b;
+        return true;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        parent.resize(n+1);
+
+        for(int i=1;i<=n;i++){
+            parent[i] =i;
+        }
+
+        for(auto edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            if(!unite(u,v)){
+                return {u,v};
+            }
+        }
+        return {};
+    }
+};
